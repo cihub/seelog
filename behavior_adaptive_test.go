69474120b215688cc2486f9f7568e1bea5aef8e4
Dmitry Bondarenko <Bond.Dmitry@gmail.com>
2012-11-02 00:04:56 +0400

    Removed common helpers test file.

--- behavior_adaptive_test.go
+++ behavior_adaptive_test.go
@@ -29,6 +29,42 @@ import (
 	"testing"
 )
 
+func countSequencedRowsInFile(filePath string) (int64, error) {
+	bts, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		return 0, err
+	}
+
+	bufReader := bufio.NewReader(bytes.NewBuffer(bts))
+
+	var gotCounter int64
+	for {
+		line, _, bufErr := bufReader.ReadLine()
+		if bufErr != nil && bufErr != io.EOF {
+			return 0, bufErr
+		}
+
+		lineString := string(line)
+		if lineString == "" {
+			break
+		}
+
+		intVal, atoiErr := strconv.ParseInt(lineString, 10, 64)
+		if atoiErr != nil {
+			return 0, atoiErr
+		}
+
+		if intVal != gotCounter {
+			return 0, errors.New(fmt.Sprintf("Wrong order: %d Expected: %d\n", intVal, gotCounter))
+		}
+
+		gotCounter++
+	}
+
+	return gotCounter, nil
+}
+
+
 func Test_Adaptive(t *testing.T) {
 	fileName := "beh_test_adaptive.log"
 	count := 100
