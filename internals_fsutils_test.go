3fcb894639b56a47ce722d1cce34db145fc39778
Marc Lallaouret <mlallaouret@gmail.com>
2016-04-08 15:11:40 +0200

    Create utils methods to handle tar and gzip

---
+++ internals_fsutils_test.go
@@ -0,0 +1,45 @@
+package seelog
+
+import (
+	"testing"
+	"fmt"
+	"reflect"
+)
+
+
+func TestGzip(t *testing.T) {
+	defer cleanupWriterTest(t)
+
+	files := make(map[string][]byte)
+	files["file1"] = []byte("I am a log")
+	err := createGzip("./gzip.gz", files["file1"])
+	if err != nil {
+		fmt.Println(err)
+		t.Fail()
+	}
+
+	decompressedFiles, err := unGzip("./gzip.gz")
+	if err != nil {
+		fmt.Println(err)
+		t.Fail()
+	}
+
+	equal := reflect.DeepEqual(files["file1"], decompressedFiles)
+	if !equal {
+		t.Fail()
+	}
+}
+
+func TestTar(t *testing.T) {
+	defer cleanupWriterTest(t)
+	files := make(map[string][]byte)
+	files["file1"] = []byte("I am a log")
+	files["file2"] = []byte("I am another log")
+	tar, _ := createTar(files)
+
+	resultFiles, _ := unTar(tar)
+	equal := reflect.DeepEqual(files, resultFiles)
+	if !equal {
+		t.Fail()
+	}
+}
